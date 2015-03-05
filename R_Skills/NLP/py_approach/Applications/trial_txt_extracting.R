111b05bc4ebfd842be3ea61ed0b0fa7f23eed395
= <=>
2015-03-05 11:13:47 -0500

    More txt extraction in R

--- R_Skills/NLP/py_approach/Applications/trial_txt_extracting.R
+++ R_Skills/NLP/py_approach/Applications/trial_txt_extracting.R
@@ -1,6 +1,8 @@
 #This is an extractor for pdfs
 #The code was compiled in Python
 require(rPython)
+require(dplyr)
+require(stringr)
 python.load("R_Skills/NLP/py_approach/Applications/txt_extractor.py")
 txt_ex <- function(x){
   output = python.call("txt_extractor",x)
@@ -8,5 +10,7 @@ txt_ex <- function(x){
 }
 
 pdf <- "/Users/Ericdunford/Desktop/psyc789w-HM/test.pdf" 
-txt_ex(pdf) 
+text <- txt_ex(pdf) %>% str_trim()
+class(text) 
+text
 # Still needs some processing, but it works!
